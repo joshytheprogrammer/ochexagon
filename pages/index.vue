18180231da07e0ed7c815fc508019c5bd8b96bf6
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-25 12:59:22 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!-- https://www.weberlogistics.com/hubfs/Chemical-warehouse-3-resized.jpg -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
