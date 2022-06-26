071fa0bc3926b62f1e6af875e4cc20b162e15ef1
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-26 19:21:54 +0100

    added default template

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <Nav />
+    <div class="body">
+      <Nuxt />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Nav from "~/components/App/Nav.vue"
+import Footer from "~/components/App/Footer.vue"
+export default {
+  components: {
+    Nav,
+    Footer
+  }
+}
+</script>
+
+<style>
+.body {
+  overflow-x: hidden;
+  margin-top: 100px;
+}
+</style>
