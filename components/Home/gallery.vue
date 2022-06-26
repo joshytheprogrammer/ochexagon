7539b50942181efd4639328eb649fe397f67d607
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-26 21:27:28 +0100

    added gallery page

---
+++ components/Home/gallery.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="gallery" id="gallery">
+    <figure>
+      <img src="https://images.unsplash.com/photo-1513828742140-ccaa28f3eda0" alt="ochexagon image 1">
+    </figure>
+    <figure>
+      <img src="https://images.unsplash.com/photo-1599727277643-b0c9cfb7705d" alt="ochexagon image 2">
+    </figure>
+    <figure>
+      <img src="https://images.unsplash.com/photo-1518152006812-edab29b069ac" alt="ochexagon image 3">
+    </figure>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.gallery {
+  margin: 72px 1rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+
+  figure {
+    height: 400px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      display: block; 
+    }
+  }
+
+  @media screen and (max-width: $m-large) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: $medium) {
+    display: block;
+    
+    figure {
+      margin: 1rem 0;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,20 @@
   <div class="home">
     <Jumbotron />
     <Services />
+    <Gallery />
   </div>
 </template>
 
 <script>
 import Jumbotron from "~/components/Home/Jumbotron.vue"
 import Services from "~/components/Home/Services.vue"
+import Gallery from "~/components/Home/gallery.vue"
+
 export default {
   components: {
     Jumbotron,
-    Services
+    Services,
+    Gallery,
   }
 }
 </script>
